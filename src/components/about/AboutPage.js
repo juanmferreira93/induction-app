42c18c01ffb50edd580c7762b72fe10b1dc472eb
= <=>
2020-01-23 13:24:01 -0300

    Create structure and first pages

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p>
+      This page use React y asdfasdfasdfasdfasdf
+      asdfasdfasdfasdf dasdfasdf
+    </p>
+  </div>
+);
+
+export default AboutPage;
